c8810b05f41eba45e9fb812086b048d403b5e222
DeboDevelop <debajyotidofficial@gmail.com>
2020-03-23 15:49:51 +0530

    Switched Back to SQLlite3 database

--- src/Endorium/Endorium/settings.py
+++ src/Endorium/Endorium/settings.py
@@ -76,12 +76,8 @@ WSGI_APPLICATION = 'Endorium.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'Endorium_database',
-        'USER': 'postgres',
-        'PASSWORD': 'root',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
