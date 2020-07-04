1ca010621b106aa0d7d7042079fbf9781f2d73b6
DeboDevelop <debajyotidofficial@gmail.com>
2020-07-04 19:25:05 +0530

    The Server is Listening on a Port

--- src/server.js
+++ src/server.js
@@ -1,1 +1,9 @@
 require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+
+const PORT = process.env.PORT || 8000;
+
+app.listen(PORT, () => console.log(`The Server is running on ${PORT}`));
