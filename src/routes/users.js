b1ba7d5d85f0f9a68dbc8f5fd4eddff00d73a7f3
DeboDevelop <debajyotidofficial@gmail.com>
2020-07-04 19:47:15 +0530

    Added a Get all function and it's working

--- src/routes/users.js
+++ src/routes/users.js
@@ -3,4 +3,34 @@ const router = express.Router();
 const User = require('../models/users');
 const { json } = require('express');
 
+//Get all
+router.get('/', async (req, res) => {
+    try {
+        const users = await User.find();
+        res.json(users);
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+});
+
+//get one
+router.get('/:id', (req, res) => {
+
+});
+
+//post one
+router.post('/', (req, res) => {
+
+});
+
+//update one
+router.patch('/:id', (req, res) => {
+
+});
+
+//delete one
+router.delete('/:id', (req, res) => {
+
+});
+
 module.exports = router;
--- src/server.js
+++ src/server.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 
 const app = express();
 
+//Connection to Database
 mongoose.connect( process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true } );
 const db = mongoose.connection;
 db.on('error', (error) => console.error(error));
@@ -11,9 +12,11 @@ db.once('open', () => console.log('Connected to Database'));
 
 app.use(express.json());
 
+//User Route
 const usersRouter = require('./routes/users');
 app.use('/api/users', usersRouter);
 
+//Listening on a port
 const PORT = process.env.PORT || 8000;
 
 app.listen(PORT, () => console.log(`The Server is running on ${PORT}`));
