1ca85c33f97d284e202f8d23d58f8961d4be3d94
DeboDevelop <debajyotidofficial@gmail.com>
2020-07-04 21:01:35 +0530

    Created the middleware function for messages and created a route to get one message by id.

--- src/routes/messages.js
+++ src/routes/messages.js
@@ -15,12 +15,24 @@ router.get('/', async (req, res) => {
 
 //get one
 router.get('/:id', getMessages, (req, res) => {
-
+    res.json(res.message);
 });
 
 //post one
 router.post('/', async (req, res) => {
+    const message = new Message({
+        message: req.body.message,
+        created_by_user: req.body.created_by_user,
+        room_no: req.body.room_no,
+        upvote: req.body.upvote
+    });
 
+    try {
+        const newMessage = await message.save();
+        res.status(201).json(newMessage);
+    } catch (err) {
+        res.status(400).json({ message: json.message });
+    }
 });
 
 //update one
@@ -35,6 +47,18 @@ router.delete('/:id', getMessages, async (req, res) => {
 
 //Middle for One Message
 async function getMessages(req, res, next) {
+    let message;
+    try {
+        message = await Message.findById(req.params.id);
+        if( message == null)
+        {
+            return res.status(404).json({ message: 'Cannot find User' })
+        }
+    } catch (err) {
+        return res.status(500).json({ message: err.message })
+    }
+
+    res.message = message;
     next();
 }
 
