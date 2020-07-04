e87607f9a06c65a0eb05456d5397d8e77ca57ecf
DeboDevelop <debajyotidofficial@gmail.com>
2020-07-04 19:34:00 +0530

    Created the user Schema and Connected to the Database.

--- src/server.js
+++ src/server.js
@@ -4,6 +4,13 @@ const mongoose = require('mongoose');
 
 const app = express();
 
+mongoose.connect( process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true } );
+const db = mongoose.connection;
+db.on('error', (error) => console.error(error));
+db.once('open', () => console.log('Connected to Database'));
+
+app.use(express.json());
+
 const PORT = process.env.PORT || 8000;
 
 app.listen(PORT, () => console.log(`The Server is running on ${PORT}`));
