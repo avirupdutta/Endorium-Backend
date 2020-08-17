b2fed382957dac38c499bf12fec5af31ffc82e76
DeboDevelop <debajyotidofficial@gmail.com>
2020-08-17 15:04:21 +0530

    Changed default value or current_room and changed devMode to False.

--- src/models/users.js
+++ src/models/users.js
@@ -21,7 +21,7 @@ const userSchema = new mongoose.Schema({
   current_room: {
     type: String,
     required: false,
-    default: null,
+    default: "",
   },
 });
 
--- src/settings.js
+++ src/settings.js
@@ -1,11 +1,7 @@
-const devMode = true;
+const devMode = false;
 
 module.exports = {
-	DATABASE_URL: devMode
-		? `mongodb://localhost:27017/Endorium?retryWrites=true&w=majority`
-		: `mongodb+srv://${process.env.dbUsername}:${
-				process.env.dbPassword
-		  }@cluster0.c9lka.mongodb.net/${
-				process.env.dbName
-		  }?retryWrites=true&w=majority`
+  DATABASE_URL: devMode
+    ? `mongodb://localhost:27017/Endorium?retryWrites=true&w=majority`
+    : `mongodb+srv://${process.env.dbUsername}:${process.env.dbPassword}@cluster0.c9lka.mongodb.net/${process.env.dbName}?retryWrites=true&w=majority`,
 };
